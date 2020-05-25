f3aec33cb58ec80506177cb83e8d921423ba0a16
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2020-05-25 10:55:07 +0200

    Added a second query for updating the tasklist priorities

--- routes/API/projects.js
+++ routes/API/projects.js
@@ -37,27 +37,33 @@ router.put('/:id/task/new', async (req, res) => {
 // Update a moved task
 router.put('/:project_id/move_task/:task_id', async (req, res) => {
 
-    const project = await Project.updateOne(
-        
-        
+    const project = await Project.updateOne(  
+        {_id: req.params.project_id},
+        {
+            $set: {'tasks.$[elementA].priority': req.body.new_state.priority, 'tasks.$[elementA].state': req.body.new_state.state},      
+        },
+        {
+            arrayFilters: [
+                {'elementA._id': req.params.task_id},
+            ]
+        }
+    );
 
-            {_id: req.params.project_id},
-            {
-                // $set: {'tasks.$[elementA].priority': req.body.new_state.priority},
-                $inc: {'tasks.$[elementB].priority': 1, 'tasks.$[elementC].priority': -1},
+    await Project.updateOne(  
+        {_id: req.params.project_id},
+        {
+            $inc: {'tasks.$[elementA].priority': 1, 'tasks.$[elementB].priority': -1},      
+        },
+        {
+            arrayFilters: [
+                {'elementA.state': req.body.new_state.state, 'elementA.priority': {gt: req.body.new_state.priority}},
+                {'elementB.state': req.body.old_state.state, 'elementB.priority': {gt: req.body.old_state.priority}},
                 
-            },
-            {
-                arrayFilters: [
-                    // {'elementA._id': req.params.task_id},
-                    {'elementB.priority': {$gt: 4}, 'elementB.state': req.body.old_state.state},
-                    {'elementC.priority': {$lt: 4}},
-                ]
-            }
-        
-    )
+            ]
+        }
+    );
 
-    
+    res.json(project);
 });
 
 router.get('/', async (req, res) => {
