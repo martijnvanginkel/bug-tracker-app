067070e172d3b5d50a531ee85dc70da50b5f8a0b
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2020-06-18 11:58:11 +0200

    Added query for shuffling a task down the same state-list

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -24,24 +24,33 @@ const shuffleTask = async (req, res) => {
                 UPDATE tasks
                 SET priority = 
                     CASE
-                        WHEN priority BETWEEN ${req.body.old_state.priority} AND ${req.body.new_state.priority - 1} THEN priority - 1
+                        WHEN priority BETWEEN ${req.body.old_state.priority + 1} AND ${req.body.new_state.priority} THEN priority - 1
                         WHEN priority = ${req.body.old_state.priority} THEN ${req.body.new_state.priority}
                     END
                 WHERE state = '${req.body.old_state.state}' AND project_id = ${req.body.project_id} AND priority BETWEEN ${req.body.old_state.priority} AND ${req.body.new_state.priority}
             `);
         }
-        // WHEN priority BETWEEN 0 AND 1 THEN priority + 1
-        // WHEN priority = ${req.body.old_state.priority} THEN ${req.body.new_state.priority}
-
-
-
-        // await connection.pool.query(`
-        //     UPDATE tasks
-        //     SET priority = priority + 1
-        //     WHERE priority >= $1 AND state = $2
-        // `, [req.body.new_state.priority, req.body.new_state.state]);
         const result = {};
-        // console.log(result[0]);
+        res.json({ result });
+    }
+    catch (error) {
+        res.status(status.error).json({ message: error.message });
+    }
+}
+
+const moveTask = async (req, res) => {
+    try {
+        console.log('move task')
+        const task = await connection.pool.query(`
+            UPDATE tasks
+            SET priority = 
+                CASE
+                    WHEN priority >= ${req.body.new_state.priority} THEN priority + 1
+                END
+            WHERE state = '${req.body.new_state.state}' AND project_id = ${req.body.project_id} AND priority >= ${req.body.new_state.priority}
+        `);
+        
+        const result = {};
         res.json({ result });
     }
     catch (error) {
